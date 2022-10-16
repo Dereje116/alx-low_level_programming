b1578aadb8fa5c326af87e9727e418fd198ef0da
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-16 03:59:36 -0700

    k

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - this is the main function
+ *description - the starting piont of the progarm
+ */
+int main(viod)
+{
+	puts("programming is like building lingualpuzzel");
+	return (0);
+}
