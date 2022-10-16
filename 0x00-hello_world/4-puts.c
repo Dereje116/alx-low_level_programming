c6492c68e5476c2c5ac4d3447a4196b4ef7acc0d
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-16 06:10:35 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-*main - this is the main function
-*description - the starting piont of the progarm
-*return : always return 0
-*/
-int main(viod)
+ * main - entry
+ *
+ * viod - the viod for main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
