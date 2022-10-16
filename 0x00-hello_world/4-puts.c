50ac45cce76c108731b909dfaef676b646b58765
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-16 04:40:58 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,10 @@
 /**
 *main - this is the main function
 *description - the starting piont of the progarm
+* return : always 0
 */
 int main(viod)
 {
-	puts ("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
