2f3bb7f4826d6dc330d811ac58082ce2efe146ab
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-18 12:39:02 -0700

    ks

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
  /**
   * print_sign - prints the sign of a number
   * @n: the num to be checked
-  * Return: 1 and peints + if n is greater than zero
+  * Return: 1 and prints + if n is greater than zero
   * 0 nd prints 0 if n is zero
   * -1 and prints - if n is less than zero
   */
