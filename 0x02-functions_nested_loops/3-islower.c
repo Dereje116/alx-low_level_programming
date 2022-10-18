43bf56bccad983d7635ae3b3500d509a16f18975
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-18 11:27:49 -0700

    sj

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - the function that determine the lower cases
+ * Return: 1 if char is lowercase, otherwise 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
