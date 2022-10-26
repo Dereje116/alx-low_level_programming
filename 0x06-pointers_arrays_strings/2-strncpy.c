8a9a502cc37648e3e9e2e3259851e35b9fd90ecd
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-26 12:51:50 -0700

    j

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,20 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
+	int i;
 
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[j] = '\0';
+		dest[j] = src[j];
+		i++;
+		j++;;
 	}
+	dest[i] = '\0';
 	return (dest);
 }
