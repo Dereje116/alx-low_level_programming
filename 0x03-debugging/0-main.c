6823b95d47a0e3eb315c216a88b060fb94fc2f62
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-30 11:03:30 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,6 @@
 #include "main.h"
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 /**
- * positive_or_negative - to function that test the number
- *
- * @i: number tobe tested
- * Return: 0;
- */
-void positive_or_negative(int i)
-{
-	int n;
-
-	srand(time(0));
-	n = i;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	if (n == 0)
-		printf("%d is zero\n", n);
-	if (n < 0)
-		printf("%d is negative\n", n);
-}
-/**
- * main - the main function
+ * main - test the filunction
  * Return: 0
  */
 
@@ -32,5 +10,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
