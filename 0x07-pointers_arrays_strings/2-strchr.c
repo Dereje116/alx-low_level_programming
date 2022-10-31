0913ce24616e772024b79c726dd4820f0710667e
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-31 12:12:13 -0700

    mew:

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strchr - strchr under s function
+ * @s: the source
+ * @c: the character
+ * Return: Null or character
+ */
+char *_strchr(char *s, char c);
+{
+	char *p = NULL;
+
+	p = strchr(s, c);
+
+	return (p);
+}
