bc4d90232224f694261a761ccd63d88aea3093b0
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-31 14:23:07 -0700

    g

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - locate a substring
+ * @haystack: string
+ * @needle: string
+ * Return: beg Null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int k = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		j = 0;
+		k = i;
+		while (haystack[k] == needle[j] && needle[j] != '\0')
+		{
+			j++;
+			i++;
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
+}
