9737de50520d3daee2ff2c6f6e7ba9c3ae9fe212
Dereje Dechasa <derejedechasa@gmail.com>
2022-10-31 12:38:37 -0700

    m

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - function gets the length of perfix
+ * @s: string
+ * @accept: substring
+ * Return: unsigned value
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\n'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				result++;
+				break;
+			}
+		}
+		if (s[j] == '\0')
+			return (result);
+	}
+	return (result);
+}
